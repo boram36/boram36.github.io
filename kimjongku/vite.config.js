ff96daf05035b6ce4112e571a639571a034f8dde
boram36 <kimbram36@gmail.com>
2025-11-26 22:52:04 +0900

    테흐트

--- kimjongku/vite.config.js
+++ kimjongku/vite.config.js
@@ -2,7 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-// vite.config.js
 export default defineConfig({
-  base: '/kimjongku/',
-});
+  plugins: [react()],
+})
